238c569cb8abe81ff48c795471791342eab3a882
Mehradad Shahidi <m.mehrdadshahidi@gmail.com>
2022-02-13 21:19:26 +0330

    Delete unnecessary plugins in fish
    - Delete bobthefish theme
    - Delete fzf files

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,28 @@
 set fish_greeting
-set PATH $PATH /usr/local/go/bin
-set PATH $PATH /home/m3d/go/bin
-set PATH $PATH /home/m3d/.cargo/bin
-set PATH $PATH /home/m3d/.yarn/bin
+set PATH $PATH /usr/local/go/bin 
+set PATH $PATH $HOME/.go/bin
+set PATH $PATH $HOME/.cargo/bin
+set PATH $PATH $HOME/.yarn/bin
+set PATH $PATH $HOME/.local/bin
+set PATH $PATH (find $HOME/.scripts/ -type d ) 
+
+# Abbreviation
 abbr -a ws 'sudo systemctl start windscribe'
 abbr -a wind 'windscribe connect'
-set EDITOR (which nvim) 
+
+
+# Enviroment Variables
+set -Ux TERMINAL kitty
+set -Ux RANGER_LOAD_DEFAULT_RC FALSE
+set -gx EDITOR (which nvim) 
+set -gx GOPATH ~/.go
 set -g theme_color_scheme dracula
 #-----------------------------------------------------
 #Aliases
 #-------------------------------------------------------
+alias ssh="kitty +kitten ssh"
 alias vim="nvim"
-alias lf lfrun
+#alias lf lfrun
 alias color "colorscript random"
 alias ll "exa --icons -l -g"
 alias tlmgr "/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode"
@@ -27,6 +38,16 @@ end
 # Source rvm Ruby Verson Manager
 function rvm
 	bass source ~/.rvm/scripts/rvm ';' rvm $argv
+end
+
+# Add icons to LF_ICONS Variable for lf
+if test -e ~/.config/lf/icons
+    set -gx LF_ICONS (sed ~/.config/lf/icons \
+    	    -e '/^[ \t]*#/d'       \
+    	    -e '/^[ \t]*$/d'       \
+    	    -e 's/[ \t]\+/=/g'     \
+    	    -e 's/$/ /')
+    set LF_ICONS (echo "$LF_ICONS" | tr '  ' ':' | sed "s/::/:/g")
 end
 
 # Run tmux on Start up
